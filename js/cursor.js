893db31ad8656a01996d1a98221cc4f8711c2d23
AkiTomoyaL <2636310675@qq.com>
2023-11-27 23:48:51 +0800

    Site updated: 2023-11-27 23:48:49

---
+++ js/cursor.js
@@ -0,0 +1,25 @@
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功(≧∀≦)ゞ",
+                    message: "若要转载最好保留原文链接ヽ(*。>Д<)o゜",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
